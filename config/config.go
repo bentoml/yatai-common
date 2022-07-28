0e85228637e2c7b13f3aa76df627f91f8b86e1a5
yetone <yetoneful@gmail.com>
2022-07-28 06:48:06 +0000

    refactor: update config

--- config/config.go
+++ config/config.go
@@ -50,56 +50,17 @@ type S3Config struct {
 	Secure     bool   `yaml:"secure"`
 }
 
-func GetS3Config(ctx context.Context, cliset *kubernetes.Clientset) (conf *S3Config, err error) {
-	configMapName := consts.KubeConfigMapNameS3Config
-	namespace := consts.KubeNamespaceYataiSystem
-
+func GetS3Config(ctx context.Context) (conf *S3Config, err error) {
 	conf = &S3Config{}
 	conf.Endpoint = os.Getenv(consts.EnvS3Endpoint)
 	conf.AccessKey = os.Getenv(consts.EnvS3AccessKey)
 	conf.SecretKey = os.Getenv(consts.EnvS3SecretKey)
 	conf.Region = os.Getenv(consts.EnvS3Region)
 	conf.BucketName = os.Getenv(consts.EnvS3BucketName)
-	secure, secureEnvExists := os.LookupEnv(consts.EnvS3Secure)
-	if secureEnvExists {
-		conf.Secure = secure == "true"
-	}
-
-	configMapCli := cliset.CoreV1().ConfigMaps(namespace)
-	configMap, err := configMapCli.Get(ctx, configMapName, metav1.GetOptions{})
-	isNotFound := k8serrors.IsNotFound(err)
-	if err != nil && !isNotFound {
-		err = errors.Wrapf(err, "failed to get config map %s in namespace %s", configMapName, namespace)
-		return
-	}
-
-	if isNotFound {
-		return
-	}
+	conf.Secure = os.Getenv(consts.EnvS3Secure) == "true"
 
 	if conf.Endpoint == "" {
-		conf.Endpoint = string(configMap.Data[consts.KubeConfigMapKeyS3ConfigEndpoint])
-	}
-	if conf.AccessKey == "" {
-		conf.AccessKey, err = getValueFromSecret(ctx, cliset, configMap, consts.KubeConfigMapKeyS3ConfigAccessKeySecretName, consts.KubeConfigMapKeyS3ConfigAccessKeySecretKey)
-		if err != nil {
-			return
-		}
-	}
-	if conf.SecretKey == "" {
-		conf.SecretKey, err = getValueFromSecret(ctx, cliset, configMap, consts.KubeConfigMapKeyS3ConfigSecretKeySecretName, consts.KubeConfigMapKeyS3ConfigSecretKeySecretKey)
-		if err != nil {
-			return
-		}
-	}
-	if conf.Region == "" {
-		conf.Region = string(configMap.Data[consts.KubeConfigMapKeyS3ConfigRegion])
-	}
-	if conf.BucketName == "" {
-		conf.BucketName = string(configMap.Data[consts.KubeConfigMapKeyS3ConfigBucketName])
-	}
-	if !secureEnvExists {
-		conf.Secure = string(configMap.Data[consts.KubeConfigMapKeyS3ConfigSecure]) == "true"
+		err = errors.Errorf("the environment variable %s is not set", consts.EnvS3Endpoint)
 	}
 
 	return
@@ -115,10 +76,7 @@ type DockerRegistryConfig struct {
 	Secure              bool   `yaml:"secure"`
 }
 
-func GetDockerRegistryConfig(ctx context.Context, cliset *kubernetes.Clientset) (conf *DockerRegistryConfig, err error) {
-	configMapName := consts.KubeConfigMapNameDockerRegistryConfig
-	namespace := consts.KubeNamespaceYataiDeploymentComponent
-
+func GetDockerRegistryConfig(ctx context.Context) (conf *DockerRegistryConfig, err error) {
 	conf = &DockerRegistryConfig{}
 	conf.BentoRepositoryName = os.Getenv(consts.EnvDockerRegistryBentoRepositoryName)
 	conf.ModelRepositoryName = os.Getenv(consts.EnvDockerRegistryModelRepositoryName)
@@ -126,45 +84,12 @@ func GetDockerRegistryConfig(ctx context.Context, cliset *kubernetes.Clientset) 
 	conf.InClusterServer = os.Getenv(consts.EnvDockerRegistryInClusterServer)
 	conf.Username = os.Getenv(consts.EnvDockerRegistryUsername)
 	conf.Password = os.Getenv(consts.EnvDockerRegistryPassword)
-	secure, secureEnvExists := os.LookupEnv(consts.EnvDockerRegistrySecure)
-	if secureEnvExists {
-		conf.Secure = secure == "true"
-	}
+	conf.Secure = os.Getenv(consts.EnvDockerRegistrySecure) == "true"
 
-	configMapCli := cliset.CoreV1().ConfigMaps(namespace)
-	configMap, err := configMapCli.Get(ctx, configMapName, metav1.GetOptions{})
-	isNotFound := k8serrors.IsNotFound(err)
-	if err != nil && !isNotFound {
-		err = errors.Wrapf(err, "failed to get config map %s in namespace %s", configMapName, namespace)
-	}
-	if isNotFound {
-		return
-	}
-
-	if conf.BentoRepositoryName == "" {
-		conf.BentoRepositoryName = string(configMap.Data[consts.KubeConfigMapKeyDockerRegistryConfigBentoRepositoryName])
-	}
-	if conf.ModelRepositoryName == "" {
-		conf.ModelRepositoryName = string(configMap.Data[consts.KubeConfigMapKeyDockerRegistryConfigModelRepositoryName])
-	}
 	if conf.Server == "" {
-		conf.Server = string(configMap.Data[consts.KubeConfigMapKeyDockerRegistryConfigServer])
+		err = errors.Errorf("the environment variable %s is not set", consts.EnvDockerRegistryServer)
 	}
-	if conf.InClusterServer == "" {
-		conf.InClusterServer = string(configMap.Data[consts.KubeConfigMapKeyDockerRegistryConfigInClusterServer])
-	}
-	if conf.Username == "" {
-		conf.Username = string(configMap.Data[consts.KubeConfigMapKeyDockerRegistryConfigUsername])
-	}
-	if conf.Password == "" {
-		conf.Password, err = getValueFromSecret(ctx, cliset, configMap, consts.KubeConfigMapKeyDockerRegistryConfigPasswordSecretName, consts.KubeConfigMapKeyDockerRegistryConfigPasswordSecretKey)
-		if err != nil {
-			return
-		}
-	}
-	if !secureEnvExists {
-		conf.Secure = string(configMap.Data[consts.KubeConfigMapKeyDockerRegistryConfigSecure]) == "true"
-	}
+
 	return
 }
 
@@ -217,31 +142,9 @@ type DockerImageBuilderConfig struct {
 	Privileged bool `yaml:"privileged"`
 }
 
-func GetDockerImageBuilderConfig(ctx context.Context, cliset *kubernetes.Clientset) (conf *DockerImageBuilderConfig, err error) {
-	configMapName := consts.KubeConfigMapNameDockerImageBuilderConfig
-	namespace := consts.KubeNamespaceYataiDeploymentComponent
-
+func GetDockerImageBuilderConfig() (conf *DockerImageBuilderConfig) {
 	conf = &DockerImageBuilderConfig{}
-	privileged, privilegedEnvExists := os.LookupEnv(consts.EnvDockerImageBuilderPrivileged)
-	if privilegedEnvExists {
-		conf.Privileged = privileged == "true"
-	}
-
-	configMapCli := cliset.CoreV1().ConfigMaps(namespace)
-	configMap, err := configMapCli.Get(ctx, configMapName, metav1.GetOptions{})
-	isNotFound := k8serrors.IsNotFound(err)
-	if err != nil && !isNotFound {
-		err = errors.Wrapf(err, "failed to get config map %s in namespace %s", configMapName, namespace)
-		return
-	}
-
-	if isNotFound {
-		return
-	}
-
-	if !privilegedEnvExists {
-		conf.Privileged = string(configMap.Data[consts.KubeConfigMapKeyDockerImageBuilderConfigPrivileged]) == "true"
-	}
+	conf.Privileged = os.Getenv(consts.EnvDockerImageBuilderPrivileged) == "true"
 
 	return
 }
--- consts/kube.go
+++ consts/kube.go
@@ -42,9 +42,6 @@ const (
 	// nolint: gosec
 	KubeLabelYataiDeployToken = "yatai.ai/deploy-token"
 
-	KubeLabelMcdAppCompType = "mcd-app-comp-type"
-	KubeLabelMcdAppCompName = "mcd-app-comp-name"
-
 	KubeLabelYataiOwnerReference = "yatai.ai/owner-reference"
 
 	KubeLabelGPUAccelerator = "gpu-accelerator"
@@ -76,33 +73,6 @@ const (
 
 	KubeCreator = "yatai"
 
-	KubeVolumeNamePermdir                            = "permdir"
-	KubeVolumeNameFastPermdir                        = "fast-permdir"
-	KubeVolumeNameHostTimezone                       = "host-timezone"
-	KubeVolumeNameMcdTracingAgentDir                 = "mcd-tracing"
-	KubeVolumeNameMcdJmxAgentDir                     = "mcd-jmx"
-	KubeVolumeMountPathPermdir                       = "/permdir"
-	KubeVolumeMountPathFastPermdir                   = "/fast_permdir"
-	KubeVolumeNameDockerSock                         = "mcd-docker-sock"
-	KubeVolumeMountPathDockerSock                    = "/var/run/docker.sock"
-	KubeVolumeNameDockerGraphStorage                 = "mcd-docker-graph-storage"
-	KubeVolumeMountPathDockerGraphStorage            = "/var/lib/docker"
-	KubeVolumeNameVarRun                             = "mcd-var-run"
-	KubeVolumeMountPathVarRun                        = "/var/run"
-	KubePersistentVolumeClaimNamePermdir             = "mcd-app-permdir"
-	KubePersistentVolumeClaimNameFastPermdir         = "mcd-app-fast-permdir"
-	KubePersistentVolumeClaimPermdirStorageClass     = "mcd-nfs"
-	KubePersistentVolumeClaimFastPermdirStorageClass = "mcd-fast-nfs"
-	KubeAliCouldStorageClassProvisioner              = "nasplugin.csi.alibabacloud.com"
-
-	KubeIngressCanaryHeader      = "mcd-canary"
-	KubeIngressCanaryHeaderValue = "always"
-
-	KubeNameMcdDns = "mcd-dns"
-
-	KubeStorageClassNameMcd       = "mcd"
-	KubeStorageClassNameLocalPath = "local-path"
-
 	KubeResourceGPUNvidia = "nvidia.com/gpu"
 
 	KubeEventResourceKindPod        = "Pod"
@@ -112,48 +82,13 @@ const (
 	KubeTaintKeyDedicatedNodeGroup = "mcd.io/dedicated-node-group"
 	KubeLabelDedicatedNodeGroup    = "mcd.io/dedicated-node-group"
 
-	KubeLabelMcdESEnable  = "mcd-es-enable"
-	KubeLabelMcdESSaveDay = "mcd-es-save-day"
-
 	KubeImageCSIDriver          = "image.csi.k8s.io"
 	KubeImageCSIDriverWarmMetal = "csi-image.warm-metal.tech"
-
-	KubeDefaultMcdResourceQuotaName = "mcd"
-
-	KubeLabelNodeResourceResizeCPU    = "mcd.io/resize-node-cpu"
-	KubeLabelNodeResourceResizePods   = "mcd.io/resize-node-pods"
-	KubeLabelNodeResourceResizeMemory = "mcd.io/resize-node-memory"
 
 	KubeConfigMapNameNetworkConfig = "network"
 
 	KubeConfigMapKeyNetworkConfigDomainSuffix = "domain-suffix"
 	KubeConfigMapKeyNetworkConfigIngressClass = "ingress-class"
-
-	KubeConfigMapNameS3Config = "s3"
-
-	KubeConfigMapKeyS3ConfigEndpoint            = "endpoint"
-	KubeConfigMapKeyS3ConfigAccessKeySecretName = "access-key-secret-name"
-	KubeConfigMapKeyS3ConfigAccessKeySecretKey  = "access-key-secret-key"
-	KubeConfigMapKeyS3ConfigSecretKeySecretName = "secret-key-secret-name"
-	KubeConfigMapKeyS3ConfigSecretKeySecretKey  = "secret-key-secret-key"
-	KubeConfigMapKeyS3ConfigRegion              = "region"
-	KubeConfigMapKeyS3ConfigBucketName          = "bucket-name"
-	KubeConfigMapKeyS3ConfigSecure              = "secure"
-
-	KubeConfigMapNameDockerRegistryConfig = "docker-registry"
-
-	KubeConfigMapKeyDockerRegistryConfigBentoRepositoryName = "bento-repository-name"
-	KubeConfigMapKeyDockerRegistryConfigModelRepositoryName = "model-repository-name"
-	KubeConfigMapKeyDockerRegistryConfigServer              = "server"
-	KubeConfigMapKeyDockerRegistryConfigInClusterServer     = "in-cluster-server"
-	KubeConfigMapKeyDockerRegistryConfigUsername            = "username"
-	KubeConfigMapKeyDockerRegistryConfigPasswordSecretName  = "password-secret-name"
-	KubeConfigMapKeyDockerRegistryConfigPasswordSecretKey   = "password-secret-key"
-	KubeConfigMapKeyDockerRegistryConfigSecure              = "secure"
-
-	KubeConfigMapNameDockerImageBuilderConfig = "docker-image-builder"
-
-	KubeConfigMapKeyDockerImageBuilderConfigPrivileged = "privileged"
 
 	KubeConfigMapNameYataiConfig = "yatai"
 
