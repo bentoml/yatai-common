8618622c206fadf5b6333f2296557082a2364df2
yetone <yetoneful@gmail.com>
2023-01-09 03:40:36 +0000

    feat: upgrade default images

--- consts/consts.go
+++ consts/consts.go
@@ -38,10 +38,10 @@ const (
 	YataiBentoDeploymentComponentApiServer = "api-server"
 	YataiBentoDeploymentComponentRunner    = "runner"
 
-	InternalImagesBentoDownloaderDefault    = "quay.io/bentoml/bento-downloader:0.0.1"
+	InternalImagesBentoDownloaderDefault    = "quay.io/bentoml/bento-downloader:0.0.3"
 	InternalImagesCurlDefault               = "quay.io/bentoml/curl:0.0.1"
 	InternalImagesKanikoDefault             = "quay.io/bentoml/kaniko:1.9.1"
-	InternalImagesMetricsTransformerDefault = "quay.io/bentoml/yatai-bento-metrics-transformer:0.0.2"
+	InternalImagesMetricsTransformerDefault = "quay.io/bentoml/yatai-bento-metrics-transformer:0.0.3"
 	InternalImagesBuildkitDefault           = "quay.io/bentoml/buildkit:master"
 	InternalImagesBuildkitRootlessDefault   = "quay.io/bentoml/buildkit:master-rootless"
 )
